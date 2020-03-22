d00c5abf5962a9395310f6e953772fb6b1748444
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-03-22 23:42:05 +0530

    minor fixes

---
+++ assets/todo.js
@@ -0,0 +1,37 @@
+//ajax with jquery
+// this  script send response to server through post and delete req and wait
+// for response
+$(document).ready(function(){
+
+  $('form').on('submit', function(){
+
+      var item = $('form input');
+      var todo = {item: item.val()};
+
+      $.ajax({
+        type: 'POST',
+        url: '/todo',
+        data: todo,
+        success: function(data,status,xhr){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+
+      return false;
+
+  });
+
+  $('li').on('click', function(){
+      var item = $(this).text().replace(/ /g, "-");
+      $.ajax({
+        type: 'DELETE',
+        url: '/todo/' + item,
+        success: function(data){
+          //do something with the data via front-end framework
+          location.reload();
+        }
+      });
+  });
+
+});
